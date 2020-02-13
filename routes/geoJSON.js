6f86183e7641d7c4800bce5ee90c66cf4b61bbd3
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-02-13 09:53:52 -0500

    Creating a ROUTE for the PostGIS functionality

---
+++ routes/geoJSON.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var pg = require('pg');
+	var geoJSON = require('express').Router();
+var fs = require('fs');
+
+var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+
+ // now convert the configruation file into the correct format -i.e. a name/value pair array
+ var configarray = configtext.split(",");
+ var config = {};
+ for (var i = 0; i<configarray.length;i++){
+ 	var split = configarray[i].split(':');
+ 	config[split[0].trim()] = split[1].trim();
+ }
+ var pool = new pg.Pool(config);
+ console.log(config);
+
+ geoJSON.route('/testGeoJSON').get(function(req, res){
+ 	res.json({message:req.originalUrl});
+ });
+
+ module.exports = geoJSON;
