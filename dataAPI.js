c5b6e3b61d2f5332b9bcea60fa607e76f92ba11a
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-02-13 09:41:15 -0500

    Add code to the server to support CROSS ORIGIN (CORS) requests, and add code to the server to log the requests on the console as they come in

--- dataAPI.js
+++ dataAPI.js
@@ -17,3 +17,19 @@ httpsServer.listen(4480);
 app.get('/',function(req, res){
 		res.send("hello world from the Data API")
 });
+
+// adding functionality to allow cross-origin queries when PhoneGap is running a server
+app.use(function(req, res, next){
+		res.setHeader("Access-Control-Allow-Origin","*");
+		res.setHeader("Access-Control-Allow-Headers","X-Requested-With");
+		res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+		next();
+});
+
+// adding functionality to log the requests
+app.use(function(req, res, next){
+		var filename = path.basename(req.url);
+		var extension = path.extname(filename);
+		console.log("The file" + filename + "was requested.");
+		next();
+});
