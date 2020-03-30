d0653976815beaa07cc95149ae2a5dddb82358b1
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-03-30 11:13:12 -0400

    extend the geoJSON code to test postgis

--- routes/geoJSON.js
+++ routes/geoJSON.js
@@ -18,4 +18,20 @@ geoJSON.route('/testGeoJSON').get(function (req,res) {
 	res.json({message:req.originalUrl}); 
 });
 
+geoJSON.get('/postgistest', function (req,res) { 
+pool.connect(function(err,client,done) { 
+	if(err){ 
+		console.log("not able to get connection "+ err); 
+		res.status(400).send(err);
+		} 
+		client.query('SELECT name FROM london_counties' ,function(err,result) { 
+			done(); 
+			if(err){ 
+				console.log(err); 
+				res.status(400).send(err); } 
+			res.status(200).send(result.rows);
+		}); 
+	});
+});
 module.exports = geoJSON;
+
