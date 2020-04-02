e59eff75aed16048ab117e48ada6c5d5fbc222b4
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-04-02 09:57:47 -0400

    Code to Insert Data

--- routes/crud.js
+++ routes/crud.js
@@ -18,5 +18,50 @@ crud.route('/testCRUD').get(function (req,res) {
 	res.json({message:req.originalUrl}); 
 });
 
+crud.post('/insertFormData',(req,res) => { 
+	console.dir(req.body);
+
+pool.connect(function(err,client,done) { 
+	if(err){ 
+		console.log("not able to get connection "+ err); 
+		res.status(400).send(err); 
+	}
+
+// pull the geometry component together 
+// note that well known text requires the points as longitude/latitude ! 
+// well known text should look like: 'POINT(-71.064544 42.28787)'
+var param1 = req.body.longitude ; 
+var param2 = req.body.latitude ;
+
+var param3 = req.body.name ; 
+var param4 = req.body.surname ; 
+var param5 = req.body.module; 
+var param6 = req.body.language; 
+var param7 = req.body.modulelist; 
+var param8 = req.body.lecturetime ; 
+var param9 = req.body.port_id ;
+
+// no need for injection prevention for st_geomfromtext as if 
+// the lat/lng values are not numbers it will not process them at all 
+// impossible to run a statement such as st_geomfromtext('POINT(delete from public.formdata')
+var geometrystring = "st_geomfromtext('POINT("+req.body.latitude+" "+req.body.longitude+")',4326)";
+var querystring = "INSERT into public.formdata(name,surname,module,language, modulelist,lecturetime, port_id,location) values ";
+	querystring += "($1,$2,$3,$4,$5,$6,$7,";
+	querystring += geometrystring + ")";
+		console.log(querystring); 
+		client.query( 
+			querystring,[param3,param4,param5,param6,param7,param8,param9],function(err,result) { 
+			done();
+			if(err){ 
+				console.log(err); 
+				res.status(400).send(err); 
+			} 
+			res.status(200).send("Form Data "+ req.body.name+ " has been inserted"); 
+		});
+	}); 
+});
+
+
+
 
 module.exports = crud;
