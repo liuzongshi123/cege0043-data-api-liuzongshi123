88f48eafdd5d5ee5094cfb1754cfe2483acf1afb
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-04-08 10:52:27 -0400

    add a function to show five closest points

--- routes/geoJSON.js
+++ routes/geoJSON.js
@@ -259,4 +259,29 @@ geoJSON.get('/questionAdded/Lastweek', function (req,res) {
     }); 
   });
 
+geoJSON.get('/FiveClosestPoint', function (req,res) {
+  console.dir(req.body);
+  pool.connect(function(err,client,done) { 
+    if(err){ 
+      console.log("not able to get connection "+ err); 
+      res.status(400).send(err); 
+    } 
+    var querystring = "SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features  FROM ";
+    querystring = querystring + "(SELECT 'Feature' As type     , ST_AsGeoJSON(lg.location)::json As geometry, ";
+    querystring = querystring + "row_to_json((SELECT l FROM (SELECT id, question_title, question_text, answer_1, answer_2, ";
+    querystring = querystring + "answer_3, answer_4, port_id, correct_answer) As l )) As properties ";
+    querystring = querystring + "FROM   (select c.* from public.quizquestions c ";
+    querystring = querystring + "inner join (select id, st_distance(a.location, st_geomfromtext('POINT("+req.body.longitude+ " "+req.body.latitude +")',4326)) as distance ";
+    querystring = querystring + "from public.quizquestions a order by distance asc limit 5) b on c.id = b.id ) as lg) As f";
+    client.query(querystring,function(err,result) { 
+      done(); 
+      if(err){ 
+        console.log(err); 
+        res.status(400).send(err); 
+      } 
+      res.status(200).send(result.rows); 
+    }); 
+    }); 
+  });
+
 module.exports = geoJSON;
